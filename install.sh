202f2b8c16c13123fdd9b9d02399d62cbb8f9919
Joshua Kimble <JoshuaKimble@users.noreply.github.com>
2019-05-01 13:36:39 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  echo "source ~/quick-bashrc/bash-profile.sh" >> ~/.bash_profile
+if [[ "$OSTYPE" == "darwin"* ]]
+  then echo "source ~/quick-bashrc/bash-profile.sh" >> ~/.bash_profile
 else
   echo "source ~/quick-bashrc/bash-profile.sh" >> ~/.bashrc
 fi
